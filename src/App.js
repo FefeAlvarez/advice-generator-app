4c97f82b051885e3c8cf1b4f8494aa9c037df6d6
Federico Alvarez <fefe.alvarez@gmail.com>
2022-04-16 19:25:44 -0300

    adding footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { AdviceContainer } from "./components/AdviceContainer/AdviceContainer";
+import { Footer } from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <AdviceContainer />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import "./Footer.css";
+export const Footer = () => {
+  return (
+    <div className="attribution">
+      <p>
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="https://github.com/FefeAlvarez">Federico Alvarez</a>
+        .
+      </p>
+    </div>
+  );
+};
