d1eca45c303e50e2d0264e6f862b230a3b00fec6
Federico Alvarez <fefe.alvarez@gmail.com>
2022-04-16 18:32:26 -0300

    creating components, calling the API

--- src/components/AdviceCard/AdviceCard.jsx
+++ src/components/AdviceCard/AdviceCard.jsx
@@ -1,12 +1,12 @@
 import "./AdviceCard.css";
 import separator from "../../img/pattern-divider-desktop.svg";
 import dice from "../../img/icon-dice.svg";
-export const AdviceCard = () => {
+export const AdviceCard = ({ id, advice, handleClick }) => {
   return (
     <div className="cardAdvice">
       <div className="mainCard">
-        <h4 className="titleCard"> numero de quote</h4>
-        <p className="textCard">"quote"</p>
+        <h4 className="titleCard"> ADVICE # {id}</h4>
+        <p className="textCard">"{advice}"</p>
         <img
           className="separatorCard"
           src={separator}
@@ -14,7 +14,7 @@ export const AdviceCard = () => {
         />
       </div>
       <div className="footerContainer">
-        <div className="footerCard">
+        <div className="footerCard" onClick={handleClick}>
           <img src={dice} alt="card dice figure detail" />
         </div>
       </div>
--- src/components/AdviceContainer/AdviceContainer.jsx
+++ src/components/AdviceContainer/AdviceContainer.jsx
@@ -1,9 +1,28 @@
 import "./AdviceContainer.css";
 import { AdviceCard } from "../AdviceCard/AdviceCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
 export const AdviceContainer = () => {
+  const [quote, setQuote] = useState({});
+  const [click, setClick] = useState(false);
+  const getQuoteInfo = async () => {
+    const response = await axios.get("https://api.adviceslip.com/advice");
+    const quoteData = response.data.slip;
+    setQuote(quoteData);
+  };
+  const handleClick = () => {
+    setClick(!click);
+  };
+  useEffect(() => {
+    getQuoteInfo();
+  }, [click]);
   return (
     <div className="cardContainer">
-      <AdviceCard />;
+      <AdviceCard
+        id={quote.id}
+        advice={quote.advice}
+        handleClick={handleClick}
+      />
     </div>
   );
 };
